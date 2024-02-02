d3943f7c25ad140da731eca80c8219443b64f5b2
mitrape <154874223+mitrape@users.noreply.github.com>
2024-02-02 19:50:30 +0330

    second

--- gifts.h
+++ gifts.h
@@ -1,9 +1,10 @@
 #ifndef UNTITLED52_GIFTS_H
 #define UNTITLED52_GIFTS_H
+
 // gift of increasing the number of walls
 void IncreaseCountWall(int *countwall) {
     srand(time(NULL));
-    int random=rand() % 3;
+    int random = rand() % 3;
     if (random == 0) {
         *countwall += 2;
     } else if (random == 1) {
@@ -14,19 +15,18 @@ void IncreaseCountWall(int *countwall) {
 }
 
 // gift of increasing the number of walls for player and decreasing the number of walls of opponent
-void IncreaseAndDecreaseCountWall(int *countwall1, int *counwall2){
+void IncreaseAndDecreaseCountWall(int *countwall1, int *counwall2) {
     srand(time(NULL));
-    if(rand()%2 == 0){
+    if (rand() % 2 == 0) {
         *countwall1++;
         *counwall2--;
-        if(*counwall2<0)
-            *counwall2=0;
-    }
-    else {
-        *countwall1+=2;
-        *counwall2-=2;
-        if(*counwall2<0)
-            *counwall2=0;
+        if (*counwall2 < 0)
+            *counwall2 = 0;
+    } else {
+        *countwall1 += 2;
+        *counwall2 -= 2;
+        if (*counwall2 < 0)
+            *counwall2 = 0;
     }
 }
 
